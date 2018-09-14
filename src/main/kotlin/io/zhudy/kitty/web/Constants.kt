84a63f57d82b019a6ffd77dccd50aa11fb11087a
kevin <kevinz@weghst.com>
2018-09-14 16:41:53 +0800

    新增常量定义

---
+++ src/main/kotlin/io/zhudy/kitty/web/Constants.kt
@@ -0,0 +1,12 @@
+package io.zhudy.kitty.web
+
+/**
+ * @author Kevin Zou (yong.zou@2339.com)
+ */
+object Constants {
+
+    const val HTTP_PATH_PARAM_NAME = "path"
+    const val HTTP_QUERY_PARAM_NAME = "query"
+    const val HTTP_FORM_PARAM_NAME = "form"
+
+}
