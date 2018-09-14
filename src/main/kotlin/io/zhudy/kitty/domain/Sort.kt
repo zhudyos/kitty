f018e9f3c78024f557157a3bc05569530f982e03
kevin <kevinz@weghst.com>
2018-09-14 16:41:07 +0800

    修改排序实体

--- src/main/kotlin/io/zhudy/kitty/domain/Sort.kt
+++ src/main/kotlin/io/zhudy/kitty/domain/Sort.kt
@@ -1,6 +1,8 @@
 package io.zhudy.kitty.domain
 
 /**
+ * 排序实体类型。
+ *
  * @author Kevin Zou (kevinz@weghst.com)
  *
  * @property orders 排序字段信息
@@ -20,6 +22,30 @@ data class Sort(
 }
 
 /**
- * 空排序。
+ * 无排序。
  */
 val unsorted = Sort(emptyList())
+
+/**
+ * 解析排序字符串。
+ */
+fun parseSort(sort: String?): Sort {
+    if (sort == null || sort.isEmpty()) {
+        return unsorted
+    }
+
+    val orders = arrayListOf<Sort.Order>()
+    sort.split(",").forEach {
+        if (it.isNotEmpty()) {
+            if (it.elementAt(0) == '-') {
+                orders.add(Sort.Order(
+                        name = it.substring(1),
+                        direction = "desc"
+                ))
+            } else {
+                orders.add(Sort.Order(name = it))
+            }
+        }
+    }
+    return Sort(orders)
+}
