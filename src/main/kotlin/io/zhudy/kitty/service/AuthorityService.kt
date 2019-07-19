0b1b6829ac6007d70b9a4482ee39e0f3c09b2de7
kevin <kevinz@weghst.com>
2019-07-19 20:04:06 +0800

    修改权限校验接口

--- src/main/kotlin/io/zhudy/kitty/service/AuthorityService.kt
+++ src/main/kotlin/io/zhudy/kitty/service/AuthorityService.kt
@@ -3,13 +3,25 @@ package io.zhudy.kitty.service
 import io.zhudy.kitty.auth.UserContext
 
 /**
+ * 用户权限校验。
+ *
  * @author Kevin Zou (kevinz@weghst.com)
  */
 interface AuthorityService {
 
     /**
-     *
+     * @property uri 请求的`uri`
+     * @property method 请求的`method`
+     * @property contentType 请求的`content-type`
      */
-    fun checkAuthority(requestUri: String, uc: UserContext): Boolean
+    data class Request(
+            val uri: String,
+            val method: String,
+            val contentType: String?
+    )
 
+    /**
+     * 校验用户是否有指定的访问权限。
+     */
+    fun checkAuthority(request: Request, uc: UserContext): Boolean
 }
